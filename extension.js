67ec65d1098eb3586fe4255dba782fb6b7b8263e
Callum Booth <callum-booth@live.co.uk>
2019-05-11 14:02:16 +0100

    wip

--- extension.js
+++ extension.js
@@ -1,29 +1,27 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
+const vscode = require('vscode')
+const fs = require('fs');
+const workspace = vscode.workspace;
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-
+const fc = require('./functionalComponent.js');
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "rs" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+	/**
+	 * Creates a react functional component
+	 */
+	let disposable = vscode.commands.registerCommand('extension.createFunctional', function () {
+		console.log(workspace);
+		fs.writeFile(workspace.rootPath + '/testFile3.js', fc, 'utf8', function (err) {
+			if (err) {
+				console.error(err); 
+				return false;
+			}
+			console.log('File created successfully');
+		})
 	});
-
 	context.subscriptions.push(disposable);
 }
 exports.activate = activate;
---
+++ functionalComponent.js
@@ -0,0 +1,13 @@
+`const React = require('react');
+
+const Name = () => {
+    return (
+        <div>
+            I'm the content now
+        </div>
+    )
+}
+
+module.exports = {
+    Name
+}`
